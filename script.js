d410269de1c6308fdcdb7972e3fde7f4ab7e1bd1
Elena Davydova <hardevich@gmail.com>
2024-01-09 20:39:09 +0300

    add classes

--- script.js
+++ script.js
@@ -1,46 +1,97 @@
 const goods = [
-  {
-    title: "Shirt",
-    price: 150,
-    img: "C:/Users/Елена/Documents/Courses/project/Images/Shirts.jpg",
-  },
-  {
-    title: "Socks",
-    price: 50,
-    img: "C:/Users/Елена/Documents/Courses/project/Images/Socks.jpg",
-  },
-  {
-    title: "Jacket",
-    price: 350,
-    img: "C:/Users/Елена/Documents/Courses/project/Images/Jackets.jpg",
-  },
-  {
-    title: "Shoes",
-    price: 250,
-    img: "C:/Users/Елена/Documents/Courses/project/Images/Shoes.jpg",
-  },
-];
-// console.log(goods);
+    {
+        title: "Shirt",
+        price: 150,
+        img: "C:/Users/Елена/Documents/Courses/project/Images/Shirts.jpg",
+    },
+    {
+        title: "Socks",
+        price: 50,
+        img: "C:/Users/Елена/Documents/Courses/project/Images/Socks.jpg",
+    },
+    {
+        title: "Jacket",
+        price: 350,
+        img: "C:/Users/Елена/Documents/Courses/project/Images/Jackets.jpg",
+    },
+    {
+        title: "Shoes",
+        price: 250,
+        img: "C:/Users/Елена/Documents/Courses/project/Images/Shoes.jpg",
+    },
+]
 
-const renderGoodsItem = (item, img = "https://placehold.it/200x150") => {
-  return `
+class GoodsItem {
+    constructor({
+        title = "Item",
+        price = 0,
+        img = "https://placehold.it/200x150",
+    }) {
+        this.title = title
+        this.price = price
+        this.img = img
+    }
+    render() {
+        return `
   <div class="goods-item">
-    <img src="${img}" alt="image" class="product-img">
+    <img src="${this.img}" alt="image" class="product-img">
     <div class="info-item">
-      <h3 class="product-title">${item.title}</h3>
-      <p class="product-price">${item.price}</p>
+      <h3 class="product-title">${this.title}</h3>
+      <p class="product-price">${this.price}</p>
       <button class="product-button">В корзину</button>
     </div>
   </div>
-  `;
-};
+  `
+    }
+}
 
-console.log(renderGoodsItem(goods));
+class GoodsList {
+    goods = []
+    fetchGoods() {
+        this.goods = goods
+    }
 
-const renderGoodsList = (list) => {
-  let goodsList = list.map((item) => renderGoodsItem(item, item.img)).join(" ");
-  // console.log(goodsList);
-  document.querySelector(".goods-list").innerHTML = goodsList;
-};
+    render() {
+        let goodsList = goods.map((item) => {
+            const goodsItem = new GoodsItem(item)
+            return goodsItem.render()
+        })
+        document.querySelector(".goods-list").innerHTML = goodsList.join("")
+    }
 
-renderGoodsList(goods);
+    calculate() {
+        // 2. Добавьте для GoodsList метод, определяющий суммарную стоимость всех товаров
+    }
+}
+
+const googsList = new GoodsList()
+googsList.fetchGoods()
+googsList.render()
+
+// Практическое задание
+// 3. * Некая сеть фастфуда предлагает несколько видов гамбургеров:
+// a. Маленький (50 рублей, 20 калорий).
+// b. Большой (100 рублей, 40 калорий).
+
+// Гамбургер может быть с одним из нескольких видов начинок (обязательно):
+// a. С сыром (+10 рублей, +20 калорий).
+// b. С салатом (+20 рублей, +5 калорий).
+// c. С картофелем (+15 рублей, +10 калорий).
+
+// Дополнительно гамбургер можно посыпать приправой (+15 рублей, +0 калорий) и полить
+// майонезом (+20 рублей, +5 калорий).
+
+// Напишите программу, рассчитывающую стоимость и калорийность гамбургера. Можно
+// использовать примерную архитектуру класса со следующей страницы, но можно использовать
+// и свою.
+
+// class Hamburger {
+// constructor(size, stuffing) { ... }
+// addTopping(topping) { // Добавить добавку }
+// removeTopping(topping) { // Убрать добавку }
+// getToppings(topping) { // Получить список добавок }
+// getSize() { // Узнать размер гамбургера }
+// getStuffing() { // Узнать начинку гамбургера }
+// calculatePrice() { // Узнать цену }
+// calculateCalories() { // Узнать калорийность }
+// }
